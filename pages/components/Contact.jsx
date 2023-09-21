3c6856fc16cc9222d69c605bba324b8a8cdc8225
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2023-09-20 20:49:04 -0400

    collab

--- pages/components/Contact.jsx
+++ pages/components/Contact.jsx
@@ -24,7 +24,7 @@ const Contact = () => {
 
   return (
     <div className='max-w-[1240px] m-auto p-4 h-screen'>
-      <h1 className='text-2xl font-bold text-center p-4'>Let's work together</h1>
+      <h1 className='text-2xl font-bold text-center p-4'>Work with me</h1>
       <form className='max-w-[600px] m-auto' onSubmit={handleSubmit}>
         <div className='grid grid-cols-2 gap-2'>
           <input name="name" className='border shadow-lg p-3' type="text" placeholder='Name' />
