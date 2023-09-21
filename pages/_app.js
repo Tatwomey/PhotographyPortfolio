a2e61170a7c9bac443ba3a8c22c2bc73279aca77
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2023-09-21 14:30:25 -0400

    changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import Navbar from "./components/Navbar";
+import Head from "next/head";  // Import the Head component
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      
       <Navbar />
       <Component {...pageProps} />
     </>
