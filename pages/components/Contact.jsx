09f2eb067a1103f05fd914c7bb6a334e595b7edc
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2023-09-25 15:42:34 -0400

    contact input

--- pages/components/Contact.jsx
+++ pages/components/Contact.jsx
@@ -38,20 +38,21 @@ const Contact = () => {
 
 
 
-  return (
-    <div className='max-w-[1240px] m-auto p-4 h-screen'>
+return (
+  <div className='max-w-[1240px] m-auto p-4 h-screen'>
     <h1 id="work-with-me" ref={workWithMeRef} className='text-2xl font-bold text-center p-4'>Work with me</h1>
     <form className='max-w-[600px] m-auto' onSubmit={handleSubmit}>
-        <div className='grid grid-cols-2 gap-2'>
-          <input name="name" className='border shadow-lg p-3' type="text" placeholder='Name' />
-          <input name="email" className='border shadow-lg p-3' type="email" placeholder='Email' />
-        </div>
-        <input name="subject" className='border shadow-lg p-3 w-full my-2' type="text" placeholder='Subject' />
-        <textarea name="message" className='border shadow-lg p-3 w-full' cols="30" rows="10" placeholder='Message'></textarea>
-        <button type="submit" className='border shadow-lg p-3 w-full mt-2'>Submit</button>
-      </form>
-    </div>
-  )
+      <div className='grid grid-cols-2 gap-2'>
+        <input style={{ color: 'black' }} name="name" className='border shadow-lg p-3' type="text" placeholder='Name' />
+        <input style={{ color: 'black' }} name="email" className='border shadow-lg p-3' type="email" placeholder='Email' />
+      </div>
+      <input style={{ color: 'black' }} name="subject" className='border shadow-lg p-3 w-full my-2' type="text" placeholder='Subject' />
+      <textarea style={{ color: 'black' }} name="message" className='border shadow-lg p-3 w-full' cols="30" rows="10" placeholder='Message'></textarea>
+      <button type="submit" className='border shadow-lg p-3 w-full mt-2'>Submit</button>
+    </form>
+  </div>
+)
 }
+
 
 export default Contact;
