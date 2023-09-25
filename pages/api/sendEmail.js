28fc7356b47c726cbd02d8c565211ee27aed272f
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2023-09-25 16:19:10 -0400

    additional cha cha changes

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
       service: 'gmail',
       auth: {
         user: 'info@trevortwomeyphoto.com',
-        pass: 'Manoloi5myfri3nd',
+        pass: 'Manoloi5myfri3nd!!'
       }
     });
 
