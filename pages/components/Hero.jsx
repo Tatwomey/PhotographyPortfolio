2586ca088faac778d18c0cf97297d1bdca661660
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2023-09-25 12:25:38 -0400

    hero update

--- pages/components/Hero.jsx
+++ pages/components/Hero.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-import Link from "next/link"; 
+import Link from "next/link";
 
 const Hero = ({ heading, message }) => {
   return (
     <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img">
       {/* Overlay */}
       <div className="absolute top-0 left-0 right-0 button-0 bg-black/70 z-[2]" />
-      <div className="p-5 text-white z-[2]  mt-[-10rem]">
+      <div className="p-5 text-white z-[2] mt-[-10rem]">
         <h2 className="text-5xl font-bold">{heading}</h2>
         <p className="py-5 text-lg sm:text-xl">{message}</p>
-        <Link href="/contact#work-with-me" className="px-8 py-2 border">
-  Book
-</Link>
-
+        <Link href="/contact#work-with-me">
+          <button className="px-8 py-2 border">Book</button>
+        </Link>
       </div>
     </div>
   );
