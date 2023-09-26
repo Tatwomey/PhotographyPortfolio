905cc1c9e28dfdae17528b3d35f4fb4b03c32049
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2023-09-25 20:27:59 -0400

    font family

--- pages/_document.js
+++ pages/_document.js
@@ -4,15 +4,13 @@ export default function Document() {
   return (
     <Html>
       <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link
           href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@1,200&display=swap"
           rel="stylesheet"
         />
-        <link rel="stylesheet" href="..." />
       </Head>
       <body>
         <Main />
