5afa2fd69fdd1f67b02e05fdf8036c402158506a
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2023-09-25 20:06:44 -0400

    Hero component button fix

--- pages/components/Hero.jsx
+++ pages/components/Hero.jsx
@@ -5,8 +5,8 @@ const Hero = () => {
   return (
     <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img">
       {/* Overlay */}
-      <div className="absolute top-0 left-0 right-0 button-0 bg-black/70 z-[2]" />
-      <div className="p-5 text-white z-[2] mt-[-10rem]">
+      <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-10" />
+      <div className="p-5 text-white z-20 mt-[-10rem]">
         <Link href="/contact#work-with-me">
           <button className="px-8 py-2 border">Book</button>
         </Link>
