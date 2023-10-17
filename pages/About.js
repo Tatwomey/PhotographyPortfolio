02edade046833abca0459ea14fca3148680a527f
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2023-10-17 17:46:10 -0400

    puncuation change

--- pages/About.js
+++ pages/About.js
@@ -8,7 +8,7 @@ const About = () => {
                 Trevor Twomey is a music and lifestyle photographer based in Brooklyn, NY. Specializing in music photography, he captures the live experience for artists and fans alike with a unique passion and style. In addition to his freelance work, Trevor is open to working directly with bands and is available for tours, bringing with him an eye for the distinct moments that define music events.
             </p>
             <p className="mt-4 text-lg leading-relaxed">
-                His work resonates with the energy and spirit of the music community and has graced various platforms and publications. Being in the heart of the music scene, Trevor's expertise is not limited to just capturing the event, but in translating the very essence of the moment into a lasting memory.
+                His work resonates with the energy and spirit of the music community and has graced various platforms and publications. Being in the heart of the music scene, Trevor&apos;s expertise is not limited to just capturing the event, but in translating the very essence of the moment into a lasting memory.
             </p>
             <p className="mt-4 text-lg leading-relaxed">
                 Trevor is available for hire in the U.S. and worldwide. To stay updated with his latest projects and journeys, follow him on his blog and <a href="https://www.instagram.com/trevortwomey/" target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">Instagram</a>.
@@ -18,3 +18,9 @@ const About = () => {
 }
 
 export default About;
+
+
+
+
+
+
