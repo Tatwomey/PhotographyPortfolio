f67ed595d0347689dd9899fba4efc1a3a7046594
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2023-10-26 14:20:16 -0400

    contact changes

--- pages/components/Contact.jsx
+++ pages/components/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'; 
+import React, { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
 
 const Contact = () => {
@@ -10,10 +10,13 @@ const Contact = () => {
     const form = e.target;
     const data = new FormData(form);
     const payload = {
-      name: data.get('name'),
+      firstName: data.get('firstName'),
+      lastName: data.get('lastName'),
       email: data.get('email'),
       subject: data.get('subject'),
-      message: data.get('message')
+      message: data.get('message'),
+      phoneNumber: data.get('phoneNumber'),
+      purpose: data.get('purpose')
     };
 
     try {
@@ -27,22 +30,25 @@ const Contact = () => {
 
   useEffect(() => {
     if (window.location.hash === '#work-with-me' && workWithMeRef.current) {
-        const element = workWithMeRef.current;
-        
-        const currentScrollPosition = element.getBoundingClientRect().top + window.scrollY;
-        const adjustedScrollPosition = currentScrollPosition * 0.30;
-        
-        window.scroll({
-          top: element.getBoundingClientRect().top + window.scrollY,
-          behavior: 'smooth'
-        });
+      window.scroll({
+        top: workWithMeRef.current.getBoundingClientRect().top + window.scrollY,
+        behavior: 'smooth'
+      });
     }
   }, []);
 
   return (
     <div className='max-w-[1240px] m-auto p-4 h-screen'>
       <h1 id="work-with-me" ref={workWithMeRef} className='text-2xl font-bold text-center p-4'>Work with me</h1>
-      
+      <div className="text-sm text-gray-500 mb-4 text-center">Purpose of email (required)</div>
+      <div className="flex justify-center space-x-4 mb-4">
+        <label>
+          <input type="radio" name="purpose" value="Business Inquiries" required /> Business Inquiries
+        </label>
+        <label>
+          <input type="radio" name="purpose" value="Print Inquiries" required /> Print Inquiries
+        </label>
+      </div>
       {isSubmitted ? (
         <div className="text-center text-xl font-bold text-white">
           {"Thank you, I'll be in touch."}
@@ -50,16 +56,31 @@ const Contact = () => {
       ) : (
         <form className='max-w-[600px] m-auto' onSubmit={handleSubmit}>
           <div className='grid grid-cols-2 gap-2'>
-            <input style={{ color: 'black' }} name="name" className='border shadow-lg p-3' type="text" placeholder='Name' />
-            <input style={{ color: 'black' }} name="email" className='border shadow-lg p-3' type="email" placeholder='Email' />
+            <div>
+              <input style={{ color: 'black' }} name="firstName" className='border shadow-lg p-3' type="text" placeholder='First Name' required />
+              <div className="text-sm text-gray-500">Required</div>
+            </div>
+            <div>
+              <input style={{ color: 'black' }} name="lastName" className='border shadow-lg p-3' type="text" placeholder='Last Name' required />
+              <div className="text-sm text-gray-500">Required</div>
+            </div>
           </div>
-          <input style={{ color: 'black' }} name="subject" className='border shadow-lg p-3 w-full my-2' type="text" placeholder='Subject' />
-          <textarea style={{ color: 'black' }} name="message" className='border shadow-lg p-3 w-full' cols="30" rows="10" placeholder='Message'></textarea>
+          <div>
+            <input style={{ color: 'black' }} name="email" className='border shadow-lg p-3 w-full my-2' type="email" placeholder='Email' required />
+            <div className="text-sm text-gray-500">Required</div>
+          </div>
+          <div>
+            <input style={{ color: 'black' }} name="phoneNumber" className='border shadow-lg p-3 w-full my-2' type="tel" placeholder='Phone Number (Optional)' />
+          </div>
+          <div>
+            <textarea style={{ color: 'black' }} name="message" className='border shadow-lg p-3 w-full' cols="30" rows="10" placeholder='Message' required></textarea>
+            <div className="text-sm text-gray-500">Required</div>
+          </div>
           <button type="submit" className='border shadow-lg p-3 w-full mt-2'>Submit</button>
         </form>
       )}
     </div>
-  )
+  );
 }
 
 export default Contact;
