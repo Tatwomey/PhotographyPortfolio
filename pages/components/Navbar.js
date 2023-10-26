630741eb4fe6d552ed9c81e86a8dc9f35fd599fb
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2023-10-26 13:59:41 -0400

    new navbar dropdown

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -4,70 +4,75 @@ import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 import { FaInstagram, FaLinkedin } from 'react-icons/fa';
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false);
-  const [color, setColor] = useState('transparent');
-  const [textColor, setTextColor] = useState('white');
+    const [nav, setNav] = useState(false);
+    const [backgroundColor, setBackgroundColor] = useState('transparent');
+    const [textColor, setTextColor] = useState('white');
 
-  const logo = "/waterlogo.PNG";
+    const logo = "/waterlogo.PNG";
 
-  useEffect(() => {
-    const changeColor = () => {
-      if (window.scrollY >= 90) {
-        setColor('black'); 
-        setTextColor('white');
-      } else {
-        setColor('transparent');
-        setTextColor('white');
-      }
+    useEffect(() => {
+        const changeColor = () => {
+            if (window.scrollY >= 90) {
+                setBackgroundColor('black');
+                setTextColor('white');
+            } else {
+                setBackgroundColor('transparent');
+                setTextColor('white');
+            }
+        };
+        window.addEventListener('scroll', changeColor);
+        return () => {
+            window.removeEventListener('scroll', changeColor);
+        };
+    }, []);
+
+    const handleNav = () => {
+        setNav(!nav);
     };
-    window.addEventListener('scroll', changeColor);
-    return () => {
-      window.removeEventListener('scroll', changeColor);
+
+    const closeNav = () => {
+        setNav(false);
     };
-  }, []);
 
-  const handleNav = () => {
-    setNav(!nav);
-  };
-
-  return (
-    <div style={{ backgroundColor: color }} className='fixed left-0 top-0 w-full z-30 ease-in duration-300'>
-      <div className='max-w-[1240px] m-auto flex justify-between items-center p-4 relative'>
-        <Link href='/#home' className='font-bold text-4xl cursor-pointer absolute left-4 top-4 lg:static'>
-          <img src={logo} alt="Logo" className="navbar-logo" />
-        </Link>
-        <div className="lg:hidden">
-          <button onClick={handleNav} className="absolute top-4 right-4">
-            {nav ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30} />}
-          </button>
+    return (
+        <div style={{ backgroundColor: backgroundColor }} className='fixed left-0 top-0 w-full z-30 ease-in duration-300'>
+            <div className='max-w-[1240px] m-auto flex justify-between items-center p-4 relative'>
+                <Link href='/#home'>
+                    <img src={logo} alt="Logo" className="navbar-logo cursor-pointer absolute left-4 top-4 lg:static" />
+                </Link>
+                <div className="lg:hidden">
+                    <button onClick={handleNav} className="absolute top-4 right-4">
+                        {nav ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30} />}
+                    </button>
+                </div>
+                <ul style={{ color: textColor }} className={`${nav ? 'flex flex-col h-screen justify-center space-y-4 w-full items-center lg:items-start' : 'hidden'} lg:flex flex-col lg:flex-row`}
+                    onClick={closeNav}>
+                    <li className='p-4'>
+                        <Link href='/music#music-photography'>Home</Link>
+                    </li>
+                    <li className='p-4'>
+                        <Link href='/about#about-trevor-twomey'>About</Link>
+                    </li>
+                    <li className='p-4'>
+                        <Link href='/music#music-photography'>Music</Link>
+                    </li>
+                    <li className='p-4'>
+                        <Link href='/contact#work-with-me'>Contact</Link>
+                    </li>
+                    <li className='p-4'>
+                        <a href="https://www.instagram.com/trevortwomey/" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+                            <FaInstagram size={24} />
+                        </a>
+                    </li>
+                    <li className='p-4'>
+                        <a href="https://www.linkedin.com/in/trevor-twomey/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+                            <FaLinkedin size={24} />
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
-        <ul style={{ color: textColor }} className={`${nav ? 'flex flex-col h-screen justify-center space-y-4 w-full items-center lg:items-start' : 'hidden'} lg:flex flex-col lg:flex-row`}>
-          <li className='p-4'>
-            <Link href='/music#music-photography'>Home</Link>
-          </li>
-          <li className='p-4'>
-            <Link href='/about#about-trevor-twomey'>About</Link>
-          </li>
-          <li className='p-4'>
-            <Link href='/music#music-photography'>Music</Link>
-          </li>
-          <li className='p-4'>
-            <Link href='/contact#work-with-me'>Contact</Link>
-          </li>
-          <li className='p-4'>
-            <a href="https://www.instagram.com/trevortwomey/" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
-              <FaInstagram size={24} />
-            </a>
-          </li>
-          <li className='p-4'>
-            <a href="https://www.linkedin.com/in/trevor-twomey/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
-              <FaLinkedin size={24} />
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Navbar;
