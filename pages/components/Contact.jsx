b5ffd59148d35e619865dd8e8ab42915ad52b995
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2023-10-26 22:43:15 -0400

    contact

--- pages/components/Contact.jsx
+++ pages/components/Contact.jsx
@@ -15,8 +15,7 @@ const Contact = () => {
       email: data.get('email'),
       subject: data.get('subject'),
       message: data.get('message'),
-      phoneNumber: data.get('phoneNumber'),
-      purpose: data.get('purpose')
+      phoneNumber: data.get('phoneNumber')
     };
 
     try {
@@ -30,9 +29,10 @@ const Contact = () => {
 
   useEffect(() => {
     if (window.location.hash === '#work-with-me' && workWithMeRef.current) {
+      const element = workWithMeRef.current;
       window.scroll({
-        top: workWithMeRef.current.getBoundingClientRect().top + window.scrollY,
-        behavior: 'smooth'
+        top: element.getBoundingClientRect().top + window.scrollY,
+        behavior: 'smooth',
       });
     }
   }, []);
@@ -40,28 +40,28 @@ const Contact = () => {
   return (
     <div className='max-w-[1240px] m-auto p-4 h-screen'>
       <h1 id="work-with-me" ref={workWithMeRef} className='text-2xl font-bold text-center p-4'>Work with me</h1>
-      <div className="text-sm text-gray-500 mb-4 text-center">Purpose of email (required)</div>
-      <div className="flex justify-center space-x-4 mb-4">
-        <label>
-          <input type="radio" name="purpose" value="Business Inquiries" required /> Business Inquiries
-        </label>
-        <label>
-          <input type="radio" name="purpose" value="Print Inquiries" required /> Print Inquiries
-        </label>
+      
+      <div className="mb-4">
+        <label className="text-xl">Purpose of email (required)</label>
+        <div>
+          <input type="checkbox" name="purpose" value="Business Inquiries" /> Business Inquiries
+          <input type="checkbox" name="purpose" value="Print Inquiries" className="ml-4" /> Print Inquiries
+        </div>
       </div>
+
       {isSubmitted ? (
         <div className="text-center text-xl font-bold text-white">
           {"Thank you, I'll be in touch."}
         </div>
       ) : (
         <form className='max-w-[600px] m-auto' onSubmit={handleSubmit}>
-          <div className='grid grid-cols-2 gap-2'>
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
             <div>
-              <input style={{ color: 'black' }} name="firstName" className='border shadow-lg p-3' type="text" placeholder='First Name' required />
+              <input style={{ color: 'black' }} name="firstName" className='border shadow-lg p-3 w-full' type="text" placeholder='First Name' required />
               <div className="text-sm text-gray-500">Required</div>
             </div>
             <div>
-              <input style={{ color: 'black' }} name="lastName" className='border shadow-lg p-3' type="text" placeholder='Last Name' required />
+              <input style={{ color: 'black' }} name="lastName" className='border shadow-lg p-3 w-full' type="text" placeholder='Last Name' required />
               <div className="text-sm text-gray-500">Required</div>
             </div>
           </div>
@@ -71,11 +71,11 @@ const Contact = () => {
           </div>
           <div>
             <input style={{ color: 'black' }} name="phoneNumber" className='border shadow-lg p-3 w-full my-2' type="tel" placeholder='Phone Number (Optional)' />
+            <div className="text-sm text-gray-500">(Optional)</div>
           </div>
-          <div>
-            <textarea style={{ color: 'black' }} name="message" className='border shadow-lg p-3 w-full' cols="30" rows="10" placeholder='Message' required></textarea>
-            <div className="text-sm text-gray-500">Required</div>
-          </div>
+          <input style={{ color: 'black' }} name="subject" className='border shadow-lg p-3 w-full my-2' type="text" placeholder='Subject' />
+          <textarea style={{ color: 'black' }} name="message" className='border shadow-lg p-3 w-full' cols="30" rows="10" placeholder='Message' required></textarea>
+          <div className="text-sm text-gray-500 mb-2">Required</div>
           <button type="submit" className='border shadow-lg p-3 w-full mt-2'>Submit</button>
         </form>
       )}
