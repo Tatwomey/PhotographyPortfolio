8d4ba3e4b97b39981ec5c61bac473c8965b982a4
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2023-11-02 15:47:21 -0400

    env

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ function throwError(envVar) {
   throw `Abort: You need to define ${envVar} in the .env file.`
 }
 
-if (!process.env.RESEND_API_KEY) return throwError('RESEND_API_KEY');
+if (!process.env.RESEND_API_KEY)  throwError('RESEND_API_KEY');
 
 module.exports = {
   images: {
