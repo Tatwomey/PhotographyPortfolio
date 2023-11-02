7361b8d782db5f0257a2277a1ab085a87ac9ef45
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2023-11-01 22:02:35 -0400

    resend api

---
+++ pages/api/send.js
@@ -0,0 +1,19 @@
+const { EmailTemplate } = require('../components/email-template');
+const { Resend } = require('resend');
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export default async (req, res) => {
+  try {
+    const data = await resend.emails.send({
+      from: 'Acme <onboarding@resend.dev>',
+      to: ['info@trevortwomeyphoto.com'],
+      subject: 'Hello world',
+      react: EmailTemplate({ firstName: 'John' }),
+    });
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+}
