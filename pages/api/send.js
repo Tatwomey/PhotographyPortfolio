e056858258d244052c14b53f1d35ba4f057c676a
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2023-11-01 23:21:30 -0400

    test

--- pages/api/send.js
+++ pages/api/send.js
@@ -1,20 +1,24 @@
-import EmailTemplate from '../components/email-template';
-
+const { EmailTemplate } = require('../components/email-template');
 const { Resend } = require('resend');
+const { NextApiRequest, NextApiResponse } = require('next');
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export default async (req, res) => {
-  try {
-    const data = await resend.emails.send({
-      from: 'Acme <onboarding@resend.dev>',
-      to: ['info@trevortwomeyphoto.com'],
-      subject: 'Hello world',
-      react: EmailTemplate({ firstName: 'John' }),
-    });
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    try {
+      const data = await resend.emails.send({
+        from: 'Acme <onboarding@resend.dev>',
+        to: ['info@trevortwomeyphoto.com'],
+        subject: 'Hello world',
+        react: EmailTemplate({ firstName: 'John' }),
+      });
 
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(400).json(error);
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  } else {
+    res.status(405).end();
   }
 }
