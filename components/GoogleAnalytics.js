a54d9601d0cd5575fc3ae4c000e7416b64bb8303
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2023-11-02 23:51:59 -0400

    gtag2

--- components/GoogleAnalytics.js
+++ components/GoogleAnalytics.js
@@ -1,39 +1,26 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
 import Script from 'next/script';
 
 const GoogleAnalytics = () => {
-  const router = useRouter();
+    return (
+        <>
+            <Script 
+                strategy="afterInteractive"
+                src="https://www.googletagmanager.com/gtag/js?id=G-H3MT6ZKKN4"
+            />
 
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      if (window.gtag) {
-        window.gtag('config', 'G-H3MT6ZKKN4', {
-          page_path: url,
-        });
-      }
-    };
-
-    router.events.on('routeChangeComplete', handleRouteChange);
-
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
-
-  return (
-    <>
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-H3MT6ZKKN4" strategy="afterInteractive" />
-      <Script strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-H3MT6ZKKN4');
-        `}
-      </Script>
-    </>
-  );
-};
+            <Script 
+                id="google-analytics-script"  // Added ID attribute here
+                strategy="afterInteractive"
+            >
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-H3MT6ZKKN4');
+                `}
+            </Script>
+        </>
+    );
+}
 
 export default GoogleAnalytics;
