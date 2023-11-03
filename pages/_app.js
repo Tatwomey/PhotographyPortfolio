abec3180c546b2daec5e9abd0b779dfe9c12a26b
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2023-11-02 23:36:57 -0400

    tagtest

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import Head from "next/head";
 import Footer from '@/components/Footer';
 import "@/styles/globals.css";
 import { NavigationProvider } from '../contexts/NavigationContext';
+import Script from 'next/script';
 
 
 
@@ -18,7 +19,19 @@ export default function App({ Component, pageProps }) {
       <NavigationProvider>
         {/* The Navbar can access the context values directly without props */}
         <Navbar />
-      
+      <Script strategy="afterinteractive" 
+      src="https://www.googletagmanager.com/gtag/js?id=G-H3MT6ZKKN4"/>
+
+      <Script id="google-analytics" strategy="afterInteractive">
+
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-H3MT6ZKKN4');
+          `}
+      </Script>
         <Component {...pageProps} />
         <Footer />
       </NavigationProvider>
