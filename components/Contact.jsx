ff80d0a66ca59aa7dc2b8994cd04fec2594dc83a
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2023-11-02 21:06:55 -0400

    new deployment

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -2,86 +2,102 @@ import React, { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
 
 const Contact = () => {
-  const workWithMeRef = useRef(null);
-  const [isSubmitted, setIsSubmitted] = useState(false);
+    const workWithMeRef = useRef(null);
+    const [isSubmitted, setIsSubmitted] = useState(false);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const form = e.target;
-    const data = new FormData(form);
-    const payload = {
-      firstName: data.get('firstName'),
-      lastName: data.get('lastName'),
-      email: data.get('email'),
-      subject: data.get('subject'),
-      message: data.get('message')
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const data = new FormData(form);
+        const payload = {
+            firstName: data.get('firstName'),
+            lastName: data.get('lastName'),
+            email: data.get('email'),
+            subject: data.get('subject'),
+            message: data.get('message')
+        };
+
+        // Manual checks for required fields
+        if (!payload.firstName || !payload.lastName || !payload.email || !payload.message) {
+            alert("Please fill in all required fields.");
+            return;
+        }
+
+        // Email validation using regex
+        const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+        if (!emailRegex.test(payload.email)) {
+            alert("Please enter a valid email address.");
+            return;
+        }
+
+        try {
+            const res = await axios.post('/api/send', payload);
+            if (res.data.error) {
+                alert("Error sending email: " + res.data.error.message);
+            } else {
+                console.log('Email sent successfully:', res.data);
+                setIsSubmitted(true);
+            }
+        } catch (error) {
+            console.error('Failed to send the email:', error.response ? error.response.data : error.message);
+            alert("Failed to send the email. Please try again later.");
+        }
     };
 
-    try {
-      const res = await axios.post('/api/send', payload);
-      console.log('Email sent successfully:', res.data);
-      setIsSubmitted(true);
-    } catch (error) {
-      console.error('Failed to send the email:', error.response ? error.response.data : error.message);
-    }
-  };    
+    useEffect(() => {
+        if (window.location.hash === '#work-with-me' && workWithMeRef.current) {
+            const element = workWithMeRef.current;
+            window.scroll({
+                top: element.getBoundingClientRect().top + window.scrollY,
+                behavior: 'smooth',
+            });
+        }
+    }, []);
 
-  useEffect(() => {
-    if (window.location.hash === '#work-with-me' && workWithMeRef.current) {
-      const element = workWithMeRef.current;
-      window.scroll({
-        top: element.getBoundingClientRect().top + window.scrollY,
-        behavior: 'smooth',
-      });
-    }
-  }, []);
-
-  return (
-    <div className='max-w-[1240px] m-auto p-4 h-screen pb-20'> 
-      <h1 id="work-with-me" ref={workWithMeRef} className='text-2xl font-bold text-center p-4'>Work with me</h1>
-      
-      <div className="flex flex-col items-center mb-4">
-        <label className="text-lg mb-2">Purpose of email (required)</label>
-        <div className="flex space-x-4">
-          <div>
-            <input type="checkbox" name="purpose" value="Business Inquiries" />
-            <span className="text-gray-500">Business Inquiries</span>
-          </div>
-          <div>
-            <input type="checkbox" name="purpose" value="Print Inquiries" />
-            <span className="text-gray-500">Print Inquiries</span>
-          </div>
-        </div>
-      </div>
-
-      {isSubmitted ? (
-        <div className="text-center text-xl font-bold text-white">
-          {"Thank you, I'll be in touch."}
-        </div>
-      ) : (
-        <form className='max-w-[600px] m-auto' onSubmit={handleSubmit}>
-          <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
-            <div>
-              <input style={{ color: 'black' }} name="firstName" className='border shadow-lg p-3 w-full' type="text" placeholder='First Name' required />
-              <div className="text-sm text-gray-500">Required</div>
+    return (
+        <div className='max-w-[1240px] m-auto p-4 h-screen pb-20'>
+            <h1 id="work-with-me" ref={workWithMeRef} className='text-2xl font-bold text-center p-4'>Work with me</h1>
+            <div className="flex flex-col items-center mb-4">
+                <label className="text-lg mb-2">Purpose of email (required)</label>
+                <div className="flex space-x-4">
+                    <div>
+                        <input type="checkbox" name="purpose" value="Business Inquiries" />
+                        <span className="text-gray-500">Business Inquiries</span>
+                    </div>
+                    <div>
+                        <input type="checkbox" name="purpose" value="Print Inquiries" />
+                        <span className="text-gray-500">Print Inquiries</span>
+                    </div>
+                </div>
             </div>
-            <div>
-              <input style={{ color: 'black' }} name="lastName" className='border shadow-lg p-3 w-full' type="text" placeholder='Last Name' required />
-              <div className="text-sm text-gray-500">Required</div>
-            </div>
-          </div>
-          <div>
-            <input style={{ color: 'black' }} name="email" className='border shadow-lg p-3 w-full my-2' type="email" placeholder='Email' required />
-            <div className="text-sm text-gray-500">Required</div>
-          </div>
-          <input style={{ color: 'black' }} name="subject" className='border shadow-lg p-3 w-full my-2' type="text" placeholder='Subject' />
-          <textarea style={{ color: 'black' }} name="message" className='border shadow-lg p-3 w-full' cols="30" rows="10" placeholder='Message' required></textarea>
-          <div className="text-sm text-gray-500 mb-2">Required</div>
-          <button type="submit" className='border shadow-lg p-3 w-full mt-2'>Submit</button>
-        </form>
-      )}
-    </div>
-  );
+            {isSubmitted ? (
+                <div className="text-center text-xl font-bold text-white">
+                    {"Thank you, I'll be in touch."}
+                </div>
+            ) : (
+                <form className='max-w-[600px] m-auto' onSubmit={handleSubmit}>
+                    <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+                        <div>
+                            <input style={{ color: 'black' }} name="firstName" className='border shadow-lg p-3 w-full' type="text" placeholder='First Name' required />
+                            <div className="text-sm text-gray-500">Required</div>
+                        </div>
+                        <div>
+                            <input style={{ color: 'black' }} name="lastName" className='border shadow-lg p-3 w-full' type="text" placeholder='Last Name' required />
+                            <div className="text-sm text-gray-500">Required</div>
+                        </div>
+                    </div>
+                    <div>
+                        <input style={{ color: 'black' }} name="email" className='border shadow-lg p-3 w-full my-2' type="email" placeholder='Email' required />
+                        <div className="text-sm text-gray-500">Required</div>
+                    </div>
+                    <input style={{ color: 'black' }} name="subject" className='border shadow-lg p-3 w-full my-2' type="text" placeholder='Subject' />
+                    <textarea style={{ color: 'black' }} name="message" className='border shadow-lg p-3 w-full' cols="30" rows="10" placeholder='Message' required></textarea>
+                    <div className="text-sm text-gray-500 mb-2">Required</div>
+                    <button type="submit" className='border shadow-lg p-3 w-full mt-2'>Submit</button>
+                </form>
+            )}
+        </div>
+    );
 }
 
 export default Contact;
