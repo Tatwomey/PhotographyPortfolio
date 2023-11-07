098f503e39edaad763551f596a0c8f4215aa79d0
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2023-11-06 23:41:15 -0500

    vercel fix

--- components/email-template.js
+++ components/email-template.js
@@ -3,8 +3,9 @@ const React = require("react");
 const EmailTemplate = ({ firstName }) => (
   <div>
     <h1>
-      Thanks for reaching out {firstName}! I'll be in touch with you shortly.
+    Thanks for reaching out {firstName}! I&apos;ll be in touch with you shortly.
     </h1>
+    
   </div>
 );
 
