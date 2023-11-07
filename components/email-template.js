9cdcdf71f220a7fd3230c673485b6f86517315d2
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2023-11-06 23:15:00 -0500

    main branch consolidation

--- components/email-template.js
+++ components/email-template.js
@@ -2,10 +2,12 @@ const React = require("react");
 
 const EmailTemplate = ({ firstName }) => (
   <div>
-    <h1>Thanks for reaching out {firstName}! I'll be in touch with you shortly.</h1>
+    <h1>
+      Thanks for reaching out {firstName}! I'll be in touch with you shortly.
+    </h1>
   </div>
 );
 
 module.exports = {
-  EmailTemplate
+  EmailTemplate,
 };
