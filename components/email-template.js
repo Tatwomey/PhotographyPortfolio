fcf24285bc486b968e7d8ac55a4144f056cc6384
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2023-11-07 10:48:26 -0500

    resend and template changes

--- components/email-template.js
+++ components/email-template.js
@@ -1,14 +1,11 @@
-const React = require("react");
+import React from "react";
 
 const EmailTemplate = ({ firstName }) => (
   <div>
     <h1>
-    Thanks for reaching out {firstName}! I&apos;ll be in touch with you shortly.
+      Thanks for reaching out, {firstName}!  I&apos;ll be in touch with you shortly.
     </h1>
-    
   </div>
 );
 
-module.exports = {
-  EmailTemplate,
-};
+export default EmailTemplate;
