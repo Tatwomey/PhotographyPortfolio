f436b502adfc1ea9ad2ce778184286d6d1db06a2
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2023-11-07 13:05:21 -0500

    change in node test

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ class MyDocument extends Document {
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
-            crossOrigin="anony"
+            crossOrigin="anonymous"
           />
           <link
             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400&display=swap"
