9d6106ed7e9dfd9850498aaf5b93d3a199d50e35
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2023-11-08 20:42:36 -0500

    send changes

--- pages/api/send.js
+++ pages/api/send.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
   });
 
   const mailOptions = {
-    from: 'Your Name <youremail@example.com>',
+    from: 'Trevortwomeyphoto <info@trevortwomeyphoto.com>',
     to: email,
     subject: subject,
     text: `Name: ${firstName}\nEmail: ${email}\nMessage: ${message}`,
