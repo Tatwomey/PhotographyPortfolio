83a00b04df3423253edf53782c8ea48a3e9467b1
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2023-11-08 20:50:33 -0500

    send changes abain

--- pages/api/send.js
+++ pages/api/send.js
@@ -41,7 +41,7 @@ export default async function handler(req, res) {
     const response = await resendClient.emails.send({
       from: "Trevor <info@trevortwomeyphoto.com>",
       to: [email], // Send confirmation to the user's email
-      subject: "Thanks for reaching out,({ firstName: firstName })!",
+      subject: "Thank you for reaching out!",
       react: EmailTemplate({ firstName: firstName }),
     });
 
