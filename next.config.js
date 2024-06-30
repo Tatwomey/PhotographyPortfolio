f1272673cb27f022a291c1cc13781a37a3174e14
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2024-06-30 14:26:44 -0400

    redeployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 // next.config.js
 require('dotenv').config();
 
-console.log('NEXT_PUBLIC_SHOPIFY_API_URL:', process.env.NEXT_PUBLIC_SHOPIFY_API_URL);
+console.log('SHOPIFY_API_ENDPOINT:', process.env.SHOPIFY_API_ENDPOINT);
 console.log('NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN:', process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN);
 
 const nextConfig = {
@@ -10,7 +10,7 @@ const nextConfig = {
     domains: ['cdn.shopify.com'], // Add your image domain here
   },
   env: {
-    NEXT_PUBLIC_SHOPIFY_API_URL: process.env.NEXT_PUBLIC_SHOPIFY_API_URL,
+    SHOPIFY_API_ENDPOINT: process.env.SHOPIFY_API_ENDPOINT,
     NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN: process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN,
   },
 };
