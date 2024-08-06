9e0705df87e15b04940af99d6095abdecb747e1a
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2024-08-06 13:23:43 -0400

    tag env change

--- components/GoogleAnalytics.js
+++ components/GoogleAnalytics.js
@@ -1,22 +1,24 @@
 import Script from 'next/script';
 
 const GoogleAnalytics = () => {
+    const gaId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
+
     return (
         <>
             <Script 
                 strategy="afterInteractive"
-                src="https://www.googletagmanager.com/gtag/js?id=G-H3MT6ZKKN4"
+                src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
             />
 
             <Script 
-                id="google-analytics-script"  // Added ID attribute here
+                id="google-analytics-script"
                 strategy="afterInteractive"
             >
                 {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
-                    gtag('config', 'G-H3MT6ZKKN4');
+                    gtag('config', '${gaId}');
                 `}
             </Script>
         </>
