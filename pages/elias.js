53bb1e8ce16e26b07a8d09232eff46d546ad8804
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2024-11-13 19:12:24 -0500

    sitemap update

--- pages/elias.js
+++ pages/elias.js
@@ -1,19 +1,50 @@
 import React, { useEffect, useRef } from "react";
 import Masonry from "react-masonry-css";
 import LightGallery from "lightgallery/react";
-import "lightgallery/css/lightgallery.css"; // Ensure CSS is correctly imported
+import "lightgallery/css/lightgallery.css";
 import "lightgallery/css/lg-zoom.css";
 import "lightgallery/css/lg-thumbnail.css";
 import lgThumbnail from "lightgallery/plugins/thumbnail";
 import lgZoom from "lightgallery/plugins/zoom";
-import Hero from "@/components/Hero"; // Verify the path to Hero component
+import Hero from "@/components/Hero";
 
 const photos = [
-    // List of photos
     { "src": "/Elias_nonpoint_nyc_trevortwomey.jpg", "type": "portrait" },
-    // Additional photos
     { "src": "/Elias_nonpoint_nyc_trevortwomey-2.jpg", "type": "portrait" },
-    // More photos can be added here
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-38.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-29.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-3.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-4.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-34.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-35.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-6.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-7.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-30.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-8.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-9.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-10.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-31.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-36.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-41.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-33.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-13.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-14.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-15.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-16.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-17.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-18.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-19.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-39.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-20.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-21.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-40.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-24.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-25.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-26.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-37.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-27.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-28-1.jpg", "type": "portrait" },
+    { "src": "/Elias_nonpoint_nyc_trevortwomey-43.jpg", "type": "portrait" },
 ];
 
 const Elias = () => {
@@ -82,7 +113,7 @@ const Elias = () => {
         dynamicEl={photos.map((photo) => ({
           src: photo.src,
           thumb: photo.src,
-          downloadUrl: photo.src // Enable direct download from the original source
+          downloadUrl: photo.src // Enables download of the original image with metadata
         }))}
       />
     </div>
