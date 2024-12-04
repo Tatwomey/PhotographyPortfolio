cbc7b2c359c68ce7c19bb625d427925f1ac70446
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2024-12-03 22:02:33 -0500

    image reformatting and republishing

--- pages/music.js
+++ pages/music.js
@@ -2,18 +2,27 @@ import React from "react";
 import Meta from "@/components/Meta";
 import Hero from "@/components/Hero";
 import Portfolio from "./Portfolio";
+import Head from "next/head";
 
 function Music() {
   return (
-    <div>
+    <>
+      <Head>
+        <meta name="creator" content="Trevor Twomey" />
+        <meta name="copyright" content="© Trevor Twomey" />
+        <meta name="description" content="Explore the dynamic portfolio of music and concert photography by Trevor Twomey." />
+        <meta name="rights" content="All Rights Reserved" />
+        <link rel="license" href="https://www.trevortwomeyphoto.com/licensing" />
+        <title>Music Photographer - Trevor Twomey</title>
+      </Head>
       <Meta
         title="Music Photographer - Trevor Twomey"
         description="Explore the dynamic portfolio of music and concert photography by Trevor Twomey."
-        keywords="music photography, music photographer, concert photographer, concert photography, band photos, live music photographer,live music performances, band photography, rock photographer, band photographer"
+        keywords="music photography, music photographer, concert photographer, concert photography, band photos, live music photographer, live music performances, band photography, rock photographer, band photographer"
       />
       <Hero />
       <Portfolio />
-    </div>
+    </>
   );
 }
 
