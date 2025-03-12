68fc88d0e84641dd708eecb0e0c7946307173d5f
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2025-03-12 14:46:54 -0400

    GA4 fix

--- components/GoogleAnalytics.js
+++ components/GoogleAnalytics.js
@@ -1,28 +1,45 @@
-import Script from 'next/script';
+import Script from "next/script";
 
 const GoogleAnalytics = () => {
-    const gaId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
+  const gaId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
 
-    return (
-        <>
-            <Script 
-                strategy="afterInteractive"
-                src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
-            />
+  if (!gaId) {
+    console.warn("Google Analytics ID is missing.");
+    return null;
+  }
 
-            <Script 
-                id="google-analytics-script"
-                strategy="afterInteractive"
-            >
-                {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${gaId}');
-                `}
-            </Script>
-        </>
-    );
-}
+  return (
+    <>
+      {/* Load GA4 Script */}
+      <Script 
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+      />
+
+      {/* Initialize GA4 */}
+      <Script 
+        id="google-analytics-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gaId}', {
+              page_path: window.location.pathname,
+            });
+
+            // Function to track custom events
+            window.trackGAEvent = (eventName, eventParams = {}) => {
+              if (window.gtag) {
+                window.gtag("event", eventName, eventParams);
+              }
+            };
+          `,
+        }}
+      />
+    </>
+  );
+};
 
 export default GoogleAnalytics;
--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,61 @@
-import React from 'react';
-import Navbar from '@/components/Navbar';
-import Head from 'next/head';
-import Footer from '@/components/Footer';
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Navbar from "@/components/Navbar";
+import Head from "next/head";
+import Footer from "@/components/Footer";
 import "@/styles/globals.css";
-import { NavigationProvider } from '@/contexts/NavigationContext';
-import { ShopProvider } from '@/contexts/shopContext';
-import GoogleAnalytics from '@/components/GoogleAnalytics';
-import Script from 'next/script';
+import { NavigationProvider } from "@/contexts/NavigationContext";
+import { ShopProvider } from "@/contexts/shopContext";
+import GoogleAnalytics from "@/components/GoogleAnalytics";
+import Script from "next/script";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const initialCartData = [];
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  // Track Page Views in GA4 on route change
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      if (window.gtag) {
+        window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID, {
+          page_path: url,
+        });
+      }
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ShopProvider initialCart={initialCartData}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
-      {/* Google Analytics Script */}
+      {/* Google Analytics */}
       <GoogleAnalytics />
 
-      <Script id="gtm-script" strategy="afterInteractive">
-        {/* GTM script here */}
-      </Script>
+      {/* Google Tag Manager */}
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-XXXXXXX');
+          `,
+        }}
+      />
 
-      {/* Add Lucky Orange Tracking Script */}
+      {/* Lucky Orange Tracking */}
       <Script 
         src="https://tools.luckyorange.com/core/lo.js?site-id=800b9eb3" 
         strategy="afterInteractive" 
@@ -42,7 +72,12 @@ function MyApp({ Component, pageProps }) {
       <SpeedInsights />
 
       <noscript>
-        {/* GTM noscript content */}
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
+          height="0"
+          width="0"
+          style="display:none;visibility:hidden"
+        ></iframe>
       </noscript>
     </ShopProvider>
   );
