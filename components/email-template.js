4b113db99c93222183743eea62946f0546bf39b0
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2025-03-24 12:51:14 -0400

    email template font size revision

--- components/email-template.js
+++ components/email-template.js
@@ -1,15 +1,20 @@
 const React = require("react");
 
 const EmailTemplate = ({ firstName }) => (
-  <div>
-    <h1>
-      Thanks for reaching out {firstName}! I&apos;ll be in touch with you shortly.
-    </h1>
+  <div style={{ fontFamily: "Arial, sans-serif", fontSize: "16px", lineHeight: "1.6", color: "#333" }}>
+    <p>Hi {firstName},</p>
+
+    <p>
+      Thanks for reaching out! I’ve received your message and will get back to you as soon as possible.
+    </p>
+
+    <p style={{ marginTop: "1.5rem" }}>
+      Best regards,<br />
+      Trevor Twomey
+    </p>
   </div>
 );
 
 module.exports = {
   EmailTemplate,
 };
-//test
-//Thiis work> 
