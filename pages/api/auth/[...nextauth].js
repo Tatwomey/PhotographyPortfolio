a6709c33c6b6c17dccee05f0a3a899c1867ddcda
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2025-04-30 22:13:37 -0400

    new provisioning

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -41,6 +41,7 @@ export default NextAuth({
           { id: "8", username: "robgarzamusic@gmail.com", password: "terminal5", allowedPages: ["/music/thievery25"] },
           { id: "8", username: "mikemazzeo94", password: "mikemazzeo94", allowedPages: ["/music/korn24"] },
           { id: "9", username: "ashtonmichael", password: "ashtonlogin", allowedPages: ["/music/korn24"] }
+          { id: "10", username: "Colleen@sopaproductions.com", password: "see_hennessy", allowedPages: ["/music/korn24"] }
         ];
 
         const user = users.find(
