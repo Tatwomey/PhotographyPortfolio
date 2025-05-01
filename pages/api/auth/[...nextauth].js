a5cef8143b5d37c69d0f51b4995a5d22a0713648
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2025-04-30 22:13:50 -0400

    new col

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -40,7 +40,7 @@ export default NextAuth({
           { id: "7", username: "trevor.a.twomey@gmail.com", password: "tatwomey", allowedPages: ["/music/korn24"] },
           { id: "8", username: "robgarzamusic@gmail.com", password: "terminal5", allowedPages: ["/music/thievery25"] },
           { id: "8", username: "mikemazzeo94", password: "mikemazzeo94", allowedPages: ["/music/korn24"] },
-          { id: "9", username: "ashtonmichael", password: "ashtonlogin", allowedPages: ["/music/korn24"] }
+          { id: "9", username: "ashtonmichael", password: "ashtonlogin", allowedPages: ["/music/korn24"] },
           { id: "10", username: "Colleen@sopaproductions.com", password: "see_hennessy", allowedPages: ["/music/korn24"] }
         ];
 
